6f4219a815db432eab616e9c59b3f76537f85dce
INSONG <mihwaiuck37@gmail.com>
2026-02-08 19:27:46 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 280 ms, Memory: 31336 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -2,184 +2,109 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dr = {-1, 0, 1, 0}; //ULDR
-    static int[] dc = {0, -1, 0, 1};
-    static int R, C;
+    static int R, C, T;
+    static final int[] dr = {-1, 1, 0, 0};
+    static final int[] dc = {0, 0, -1, 1};
 
     static boolean inRange(int r, int c) {
-        return r >= 0 && r < R && c >= 0 && c < C;
+        return 0 <= r && r < R && 0 <= c && c < C;
+    }
+
+    // 1) 확산 단계
+    static int[][] diffuse(int[][] map, int upper, int lower) {
+        int[][] next = new int[R][C];
+        next[upper][0] = -1;
+        next[lower][0] = -1;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (map[r][c] <= 0) continue; // 0 or -1
+
+                int amount = map[r][c] / 5;
+                if (amount == 0) {
+                    next[r][c] += map[r][c];
+                    continue;
+                }
+
+                int spreadCnt = 0;
+                for (int d = 0; d < 4; d++) {
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+                    if (!inRange(nr, nc) || map[nr][nc] == -1) continue;
+                    next[nr][nc] += amount;
+                    spreadCnt++;
+                }
+                next[r][c] += map[r][c] - amount * spreadCnt;
+            }
+        }
+        return next;
+    }
+
+    // 2) 공기청정기 순환 단계 (BOJ 17144 표준 시뮬레이션)
+    static void circulate(int[][] a, int upper, int lower) {
+        // 위쪽(반시계)
+        // 1) 왼쪽 세로(위로)
+        for (int r = upper - 1; r > 0; r--) a[r][0] = a[r - 1][0];
+        // 2) 위쪽 가로(왼쪽)
+        for (int c = 0; c < C - 1; c++) a[0][c] = a[0][c + 1];
+        // 3) 오른쪽 세로(아래로)
+        for (int r = 0; r < upper; r++) a[r][C - 1] = a[r + 1][C - 1];
+        // 4) 아래쪽 가로(오른쪽)
+        for (int c = C - 1; c > 1; c--) a[upper][c] = a[upper][c - 1];
+        a[upper][1] = 0;
+        a[upper][0] = -1;
+
+        // 아래쪽(시계)
+        // 1) 왼쪽 세로(아래로)
+        for (int r = lower + 1; r < R - 1; r++) a[r][0] = a[r + 1][0];
+        // 2) 아래쪽 가로(왼쪽)
+        for (int c = 0; c < C - 1; c++) a[R - 1][c] = a[R - 1][c + 1];
+        // 3) 오른쪽 세로(위로)
+        for (int r = R - 1; r > lower; r--) a[r][C - 1] = a[r - 1][C - 1];
+        // 4) 위쪽 가로(오른쪽)
+        for (int c = C - 1; c > 1; c--) a[lower][c] = a[lower][c - 1];
+        a[lower][1] = 0;
+        a[lower][0] = -1;
+    }
+
+    static int sumDust(int[][] map) {
+        int sum = 0;
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (map[r][c] > 0) sum += map[r][c];
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+
         int[][] map = new int[R][C];
-        int T = Integer.parseInt(st.nextToken());
-        int air = 0;
+        int upper = -1, lower = -1;
 
-        for(int i = 0; i < R; i++){
+        for (int r = 0; r < R; r++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < C; j++){
-                int n = Integer.parseInt(st.nextToken());
-                map[i][j] = n;
-                if(n == -1) air = i;
-
+            for (int c = 0; c < C; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+            // 공기청정기는 항상 1열(0번 열)에 있고, 두 줄 연속
+            if (map[r][0] == -1) {
+                if (upper == -1) upper = r;
+                else lower = r;
             }
         }
 
-        for(int t = 0; t < T; t++){
-            int[][] tempMap = new int[R][C];
-            for(int i = 0; i < R; i++){
-                for(int j = 0; j < C; j++){
-                    if(map[i][j] == -1) continue;
-                    int dust = map[i][j];
-                    for(int d = 0; d < 4; d++){
-                        int nr = i + dr[d];
-                        int nc = j + dc[d];
-                        if(inRange(nr, nc) && map[nr][nc] != -1){
-                            tempMap[nr][nc] += dust/5;
-                            map[i][j] -= dust/5;
-                        }
-                    }
-                    tempMap[i][j] += map[i][j];
-                }
-            }
-
-            int r = air - 1 , c = 1;
-            int tempDust = tempMap[r][c];
-            tempMap[r][c] = 0;
-            while(inRange(r, c)){
-                int nr = r + dr[3];
-                int nc = c + dc[3];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                     tempMap[nr][nc] = tempDust;
-                     tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-            r -= dr[3];
-            c -= dc[3];
-            while(inRange(r, c)){
-                int nr = r + dr[0];
-                int nc = c + dc[0];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                    tempMap[nr][nc] = tempDust;
-                    tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-            r -= dr[0];
-            c -= dc[0];
-            while(inRange(r, c)){
-                int nr = r + dr[1];
-                int nc = c + dc[1];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                    tempMap[nr][nc] = tempDust;
-                    tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-            r -= dr[1];
-            c -= dc[1];
-
-            while(r != air - 1){
-                int nr = r + dr[2];
-                int nc = c + dc[2];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                    tempMap[nr][nc] = tempDust;
-                    tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-
-            r = air; c = 1;
-            tempDust = tempMap[r][c];
-            tempMap[r][c] = 0;
-
-            while(inRange(r, c)){
-                int nr = r + dr[3];
-                int nc = c + dc[3];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                    tempMap[nr][nc] = tempDust;
-                    tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-            r -= dr[3];
-            c -= dc[3];
-            while(inRange(r, c)){
-                int nr = r + dr[2];
-                int nc = c + dc[2];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                    tempMap[nr][nc] = tempDust;
-                    tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-            r -= dr[2];
-            c -= dc[2];
-            while(inRange(r, c)){
-                int nr = r + dr[1];
-                int nc = c + dc[1];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                    tempMap[nr][nc] = tempDust;
-                    tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-            r -= dr[1];
-            c -= dc[1];
-
-            while(r != air - 1){
-                int nr = r + dr[0];
-                int nc = c + dc[0];
-                if(inRange(nr, nc)){
-                    int tempNextDust = tempMap[nr][nc];
-                    tempMap[nr][nc] = tempDust;
-                    tempDust = tempNextDust;
-
-                }
-                r = nr;
-                c = nc;
-            }
-            tempMap[air - 1][0] = -1;
-            tempMap[air][0] = -1;
-            map = tempMap;
+        for (int t = 0; t < T; t++) {
+            map = diffuse(map, upper, lower);
+            circulate(map, upper, lower);
         }
-        int ans = 0;
-        for(int i = 0; i < R; i++){
-            for(int j = 0; j < C; j++){
-                ans += map[i][j];
-            }
-        }
-        System.out.println(ans + 2);
 
-
-
+        System.out.println(sumDust(map));
     }
 }
