65ab17a751e45344ea8504398244e0bd13fbf3e3
INSONG <mihwaiuck37@gmail.com>
2026-02-21 23:58:51 +0900

    [Bronze V] Title: A+B - 9, Time: 208 ms, Memory: 17412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -1,14 +1,15 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 public class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
+        BigDecimal a = new BigDecimal(st.nextToken());
+        BigDecimal b = new BigDecimal(st.nextToken());
         
-        System.out.println(x + y);
+        System.out.println(a.add(b));
     }
 }
